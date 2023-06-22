a2c58e6cd567bd3f4f159b28231a805a2c74af1e
Abhishek mishra <70240207+abhishek2914@users.noreply.github.com>
2023-06-22 14:20:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,143 @@
-import argparse
-from pathlib import Path
-from src.entry import entry_point
-from src.logger import logger
+import cv2
+import numpy as np
+import utlis
+
+########################
+path="1.jpeg"
+widthImg=700
+heightImg=700
+questions=5
+choices=5
+ans=[1,2,0,1,4]
+webcamFeed = True
+cameraNo=0
+#######################
+
+cap=cv2.VideoCapture(cameraNo)
+cap.set(10,150)
+
+while True:
+    if webcamFeed:success,img=cap.read()
+    else:img=cv2.imread (path)
 
 
-def parse_args():
-    # construct the argument parse and parse the arguments
-    argparser = argparse.ArgumentParser()
+    #PROCESSING
+    img=cv2.resize(img,(widthImg,heightImg))
+    imgContours=img.copy()
+    imgFinal = img.copy()
+    imgBiggestcontours = img.copy()
+    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur=cv2.GaussianBlur((imgGray),(5,5),1)
+    imgCanny=cv2.Canny(imgBlur,10,50)
 
-    argparser.add_argument(
-        "-i",
-        "--inputDir",
-        default=["inputs"],
-        # https://docs.python.org/3/library/argparse.html#nargs
-        nargs="*",
-        required=False,
-        type=str,
-        dest="input_paths",
-        help="Specify an input directory.",
-    )
+    try:
+        #FINDING ALL CONTOURS
+        contours,hierarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(imgContours,contours,-1,(0,255,0),10)
 
-    argparser.add_argument(
-        "-o",
-        "--outputDir",
-        default="outputs",
-        required=False,
-        dest="output_dir",
-        help="Specify an output directory.",
-    )
-
-    argparser.add_argument(
-        "-a",
-        "--autoAlign",
-        required=False,
-        dest="autoAlign",
-        action="store_true",
-        help="(experimental) Enables automatic template alignment - \
-        use if the scans show slight misalignments.",
-    )
-
-    argparser.add_argument(
-        "-l",
-        "--setLayout",
-        required=False,
-        dest="setLayout",
-        action="store_true",
-        help="Set up OMR template layout - modify your json file and \
-        run again until the template is set.",
-    )
-
-    (
-        args,
-        unknown,
-    ) = argparser.parse_known_args()
-
-    args = vars(args)
-
-    if len(unknown) > 0:
-        logger.warning(f"\nError: Unknown arguments: {unknown}", unknown)
-        argparser.print_help()
-        exit(11)
-    return args
+        rectCon=utlis.rectContour(contours)
+        biggestContour=utlis.getCornerPoints(rectCon[0])
+        gradePoints = utlis.getCornerPoints(rectCon [1])
+        #print(biggestContour)
 
 
-def entry_point_for_args(args):
-    for root in args["input_paths"]:
-        entry_point(
-            Path(root),
-            args,
-        )
+        if biggestContour.size != 0 and gradePoints.size != 0:
+            cv2.drawContours(imgBiggestcontours,biggestContour,-1,(0,255,0),20)
+            cv2.drawContours(imgBiggestcontours, gradePoints,-1, (255, 0, 0), 20)
+
+            biggestContour=utlis.reorder(biggestContour)
+            gradePoints=utlis.reorder(gradePoints)
+
+            pt1 = np.float32(biggestContour)
+            pt2 = np.float32([[0,0],[widthImg,0],[0,heightImg],[widthImg,heightImg]])
+            matrix = cv2.getPerspectiveTransform(pt1,pt2)
+            imgWarpColored = cv2.warpPerspective(img,matrix,(widthImg,heightImg))
+
+            ptG1 = np.float32(gradePoints)
+            ptG2 = np.float32([[0, 0], [325, 0], [0, 150], [325, 150]])
+            matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
+            imgGradeDisplay = cv2.warpPerspective(img, matrixG, (325, 150))
+            #cv2.imshow("Grade",imgGradeDisplay)
+
+            imgWarpGray=cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+            imgThresh=cv2.threshold(imgWarpGray,150,255,cv2.THRESH_BINARY_INV)[1]
+
+            boxes = utlis.splitBoxes(imgThresh)
+            #cv2.imshow("Test",boxes[2])
+            #print(cv2.countNonZero(boxes[1]),cv2.countNonZero(boxes[2]))
+
+            #GETTING NON ZERO PIXEL VALUES OF EACH BOX
+            myPixelVal=np.zeros((questions,choices))
+            countC = 0
+            countR = 0
+
+            for image in boxes:
+                totalPixels=cv2.countNonZero(image)
+                myPixelVal[countR][countC]=totalPixels
+                countC+=1
+                if(countC==choices):countR +=1 ;countC=0
+            #print(myPixelVal)
+
+            #FINDING INDEX VALUES OF THE MARKINGS
+            myIndex = []
+            for x in range (0,questions):
+                arr=myPixelVal[x]
+                #print("arr",arr)
+                myIndexVal = np.where(arr==np.amax(arr))
+                #print(myIndexVal[0])
+                myIndex.append(myIndexVal[0][0])
+            #print(myIndex)
+
+            #GRADING
+            grading=[]
+            for x in range (0,questions):
+                if ans[x]==myIndex[x]:
+                    grading.append(1)
+                else:grading.append(0)
+            #print(grading)
+
+            score= (sum(grading)/questions)*100 #FINAL_SCORE
+            print(score)
+
+            #DISPLAYING ANSWERS
+            imgResult = imgWarpColored.copy()
+            imgResult = utlis.showAnswers(imgResult,myIndex,grading,ans,questions,choices)
+            imRawDrawing = np.zeros_like(imgWarpColored)
+            imRawDrawing = utlis.showAnswers(imRawDrawing, myIndex, grading, ans, questions, choices)
+            invmatrix = cv2.getPerspectiveTransform(pt2, pt1)
+            imgInvWarp = cv2.warpPerspective(imRawDrawing, invmatrix, (widthImg, heightImg))
+
+            imgRawGrade=np.zeros_like(imgGradeDisplay)
+            cv2.putText(imgRawGrade,str(int(score))+"%", (60,100), cv2.FONT_HERSHEY_COMPLEX,3,(0,255 ,255),3)
+            #cv2.imshow("Grade",imgRawGrade)
+            invMatrixG = cv2.getPerspectiveTransform(ptG2, ptG1)
+            imgInvGradeDisplay = cv2.warpPerspective(imgRawGrade, invMatrixG, (widthImg, heightImg))
+
+            imgFinal = cv2.addWeighted(imgFinal,1,imgInvWarp,1,0)
+            imgFinal = cv2.addWeighted(imgFinal, 1,imgInvGradeDisplay, 1, 0)
 
 
-if __name__ == "__main__":
-    args = parse_args()
-    entry_point_for_args(args)
+
+
+
+        imgBlank=np.zeros_like(img)
+        imageArray = ([img,imgGray,imgBlur,imgCanny],
+                      [imgContours,imgBiggestcontours,imgWarpColored,imgThresh],
+                      [imgResult,imRawDrawing,imgInvWarp, imgFinal])
+    except:
+        imgBlank = np.zeros_like(img)
+        imageArray = ([img, imgGray, imgBlur, imgCanny],
+                      [imgBlank,imgBlank, imgBlank, imgBlank],
+                      [imgBlank,imgBlank, imgBlank, imgBlank])
+
+
+    lables = [["Original","Gray","Blur","Canny"],
+               ["Contours","Biggest Con","Warp","Threshold"],
+               ["Result","Raw Drawing","Inv Warp","Final"]]
+    imgStacked=utlis.stackImages(imageArray,0.3)
+
+    cv2.imshow("Final Result",imgFinal)
+    cv2.imshow("stacked images",imgStacked)
+    if cv2.waitKey(1) & 0xFF==ord('s'):
+        cv2.imwrite("FinalResult.jpg",imgFinal)
+        cv2.waitKey(300)
